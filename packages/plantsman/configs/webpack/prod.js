67abd9399b4a510dadbb2b149d097aaff2559c9d
Vladyslav Sitalo <sitalov@amazon.com>
2021-04-04 15:43:58 -0700

    Webpack update config to build a single chunk

--- packages/plantsman/configs/webpack/prod.js
+++ packages/plantsman/configs/webpack/prod.js
@@ -1,17 +1,22 @@
 // production config
-const { merge } = require("webpack-merge");
-const { resolve } = require("path");
+const { merge } = require("webpack-merge")
+const { resolve } = require("path")
+const webpack = require("webpack")
 
-const commonConfig = require("./common");
+const commonConfig = require("./common")
 
 module.exports = merge(commonConfig, {
   mode: "production",
   entry: "./index.tsx",
   output: {
-    filename: "js/bundle.[contenthash].min.js",
+    filename: "js/plantsman.min.js",
     path: resolve(__dirname, "../../dist"),
     publicPath: "/",
   },
   devtool: "source-map",
-  plugins: [],
-});
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
+})
