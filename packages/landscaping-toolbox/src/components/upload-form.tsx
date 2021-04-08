f9dab00de1a527abc6ead78591ae189ae8a9f919
Vladyslav Sitalo <sitalov@amazon.com>
2021-04-08 15:31:01 -0700

    Progress indication - move text to center

--- packages/landscaping-toolbox/src/components/upload-form.tsx
+++ packages/landscaping-toolbox/src/components/upload-form.tsx
@@ -17,197 +17,200 @@ import {CssEditor} from "./css-editor"
 // import {SubscriptionModal} from "../components/subscription-modal"
 
 interface UploadFormProps {
-    allPageNames: string[]
-    roamDataSupplier?: () => RoamPage[]
+  allPageNames: string[]
+  roamDataSupplier?: () => RoamPage[]
 }
 
 const progressIndicator = (processingState: string) => <Flex sx={{
-    alignItems: "center"
+  alignItems: "center",
 }}>
-    <Spinner/>
-    <Box sx={{textAlign: "center"}}>{processingState}</Box>
+  <Spinner/>
+  <Box sx={{
+    textAlign: "center",
+    flexGrow: 1,
+  }}>{processingState}</Box>
 </Flex>
 
 
 export const UploadForm = ({allPageNames, roamDataSupplier}: UploadFormProps) => {
-    const [titlePlaceholder, setTitlePlaceholder] = useState("")
-    const [title, setTitle] = useLocalState("title", "")
-    const [publicTags, setPublicTags] = useLocalState<Tag[]>("publicTags", [{id: 0, name: "make-public"}])
-    const [privateTags, setPrivateTags] = useLocalState<Tag[]>("privateTags", [])
-    const [entryPageTag, setEntryPageTag] = useLocalState<Tag[]>("entryPageTag", [])
-    const [allPagesPublic, setAllPagesPublic] = useLocalState("allPagesPublic", false)
-    const [processingState, setProcessingState] = useState("")
-    const [file, setFile] = useState<File | undefined>(undefined)
-    const [cssCode, setCssCode] = useLocalState("cssCode", "")
-    const [cssValid, setCssValid] = useState(false)
+  const [titlePlaceholder, setTitlePlaceholder] = useState("")
+  const [title, setTitle] = useLocalState("title", "")
+  const [publicTags, setPublicTags] = useLocalState<Tag[]>("publicTags", [{id: 0, name: "make-public"}])
+  const [privateTags, setPrivateTags] = useLocalState<Tag[]>("privateTags", [])
+  const [entryPageTag, setEntryPageTag] = useLocalState<Tag[]>("entryPageTag", [])
+  const [allPagesPublic, setAllPagesPublic] = useLocalState("allPagesPublic", false)
+  const [processingState, setProcessingState] = useState("")
+  const [file, setFile] = useState<File | undefined>(undefined)
+  const [cssCode, setCssCode] = useLocalState("cssCode", "")
+  const [cssValid, setCssValid] = useState(false)
 
-    useEffect(() => {
-        (async () => {
-            const user: CognitoUser = await Auth.currentAuthenticatedUser()
-            setTitlePlaceholder(`${user.getUsername()}'s Garden`)
-        })()
-    }, [])
+  useEffect(() => {
+    (async () => {
+      const user: CognitoUser = await Auth.currentAuthenticatedUser()
+      setTitlePlaceholder(`${user.getUsername()}'s Garden`)
+    })()
+  }, [])
 
-    const allPageTags = allPageNames.map(it => ({name: it, id: it}))
+  const allPageTags = allPageNames.map(it => ({name: it, id: it}))
 
-    return (
-        // <SubscriptionModal/>
-        <Box as="section" id="upload" sx={{
-            " label": {
-                marginBottom: "0.7em",
-            }
-        }}>
-            <Container>
-                <Box
-                  as='form'
-                  onSubmit={submit}>
-                    <Label htmlFor='title'>Garden Title *</Label>
-                    <Input
-                      name="title"
-                      mb={3}
-                      value={title}
-                      onChange={event => setTitle(event.target.value)}
-                      placeholder={titlePlaceholder}
-                      required
-                    />
-                    <Label htmlFor='entry'>Starting page *</Label>
-                    <TagInput minTags={1}
-                              maxTags={1}
-                              suggestions={allPageTags}
-                              tags={entryPageTag}
-                              setTags={setEntryPageTag}
-                              placeholderText="About these notes"/>
+  return (
+    // <SubscriptionModal/>
+    <Box as="section" id="upload" sx={{
+      " label": {
+        marginBottom: "0.7em",
+      },
+    }}>
+      <Container>
+        <Box
+          as="form"
+          onSubmit={submit}>
+          <Label htmlFor="title">Garden Title *</Label>
+          <Input
+            name="title"
+            mb={3}
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+            placeholder={titlePlaceholder}
+            required
+          />
+          <Label htmlFor="entry">Starting page *</Label>
+          <TagInput minTags={1}
+                    maxTags={1}
+                    suggestions={allPageTags}
+                    tags={entryPageTag}
+                    setTags={setEntryPageTag}
+                    placeholderText="About these notes"/>
 
-                    <Label>
-                        <Checkbox
-                          checked={allPagesPublic}
-                          onChange={event => setAllPagesPublic(event.target.checked)}
-                        />
-                        Make All pages public
-                    </Label>
+          <Label>
+            <Checkbox
+              checked={allPagesPublic}
+              onChange={event => setAllPagesPublic(event.target.checked)}
+            />
+            Make All pages public
+          </Label>
 
-                    <Label>Make pages with these tags public (all pages are private by default) *</Label>
-                    <TagInput minTags={1}
-                              suggestions={allPageTags}
-                              tags={publicTags}
-                              setTags={setPublicTags}
-                              disabled={allPagesPublic}/>
+          <Label>Make pages with these tags public (all pages are private by default) *</Label>
+          <TagInput minTags={1}
+                    suggestions={allPageTags}
+                    tags={publicTags}
+                    setTags={setPublicTags}
+                    disabled={allPagesPublic}/>
 
-                    <Label>Make blocks with these tags private</Label>
-                    <TagInput tags={privateTags}
-                              suggestions={allPageTags}
-                              setTags={setPrivateTags}/>
+          <Label>Make blocks with these tags private</Label>
+          <TagInput tags={privateTags}
+                    suggestions={allPageTags}
+                    setTags={setPrivateTags}/>
 
-                    {!roamDataSupplier && <>
-                        <Label htmlFor='db'>Your graph JSON (
-                            <A
-                              href="https://roamresearch.freshdesk.com/support/solutions/articles/64000248331-how-to-export-your-roam-graph">
-                                how to get it</A>)
-                        </Label>
-                        <Input
-                          type='file'
-                          name='db'
-                          mb={3}
-                          accept='application/json'
-                          onChange={
-                              e => setFile(e.target.files?.[0])
-                          }
-                          required
-                        />
-                    </>}
+          {!roamDataSupplier && <>
+              <Label htmlFor="db">Your graph JSON (
+                  <A
+                      href="https://roamresearch.freshdesk.com/support/solutions/articles/64000248331-how-to-export-your-roam-graph">
+                      how to get it</A>)
+              </Label>
+              <Input
+                  type="file"
+                  name="db"
+                  mb={3}
+                  accept="application/json"
+                  onChange={
+                    e => setFile(e.target.files?.[0])
+                  }
+                  required
+              />
+          </>}
 
-                    <Label>Custom CSS (optional)</Label>
-                    {CssEditor({cssCode, setCssCode, cssValid, setCssValid})}
+          <Label>Custom CSS (optional)</Label>
+          {CssEditor({cssCode, setCssCode, cssValid, setCssValid})}
 
-                    {processingState ? progressIndicator(processingState) :
-                      <Button disabled={!isValid()}>Submit</Button>}
-                </Box>
-            </Container>
+          {processingState ? progressIndicator(processingState) :
+            <Button disabled={!isValid()}>Submit</Button>}
         </Box>
-    )
+      </Container>
+    </Box>
+  )
 
 
-    function isValid() {
-        let result = true
-        if (!(publicTags.length || allPagesPublic)) {
-            result = false
-            console.log("Must specify at least one public tag")
-        }
-
-        if (!entryPageTag.length) {
-            result = false
-            console.log("Must specify entry page")
-        }
-
-        if (!file && !roamDataSupplier) {
-            result = false
-            console.log("Must select file")
-        }
-
-        if (!title) {
-            result = false
-            console.log("Must enter title")
-        }
-
-        if (!cssValid) {
-            result = false
-            console.log("CSS is invalid")
-        }
-
-        return result
+  function isValid() {
+    let result = true
+    if (!(publicTags.length || allPagesPublic)) {
+      result = false
+      console.log("Must specify at least one public tag")
     }
 
-    async function getDataToUpload() {
-        if (!roamDataSupplier) {
-            return file
-        }
-
-        setProcessingState("Creating full data export")
-        const roamExport = await executeAfterDelay(0, roamDataSupplier)
-        console.log("Export done, performing filtering")
-        const {pages} = new RoamJsonQuery(roamExport, constructFilter()).getPagesToRender()
-        console.log("Export & filtering done")
-        return JSON.stringify(pages)
+    if (!entryPageTag.length) {
+      result = false
+      console.log("Must specify entry page")
     }
 
-    function constructFilter() {
-        return {
-            makeAllPagesPublic: allPagesPublic,
-            pagesToMakePublic: [entryPageName()],
-            makePagesWithTheseTagsPublic: publicTags.map(it => it.name),
-            makeBlocksWithTheseTagsPrivate: privateTags.map(it => it.name),
-        }
+    if (!file && !roamDataSupplier) {
+      result = false
+      console.log("Must select file")
     }
 
-    async function submit(e: FormEvent<HTMLDivElement>) {
-        e.preventDefault()
-        if (!isValid()) return
-
-        setProcessingState("Initiating upload")
-        const roamData = await getDataToUpload()
-        setProcessingState("Uploading the export to Roam Garden")
-        const [url, cssUrl] = await Promise.all([upload(roamData), upload(cssCode)])
-        const entryPage = entryPageName()
-        const payload = {
-            config: {
-                filter: constructFilter(),
-                siteTitle: title,
-                entryPage,
-                cssUrl,
-            },
-            dbUrl: url,
-        }
-        console.log("sending", payload)
-      // todo don't commit
-        const result = await API.put(gardenApi, "/garden", {body: payload,})
-        console.log(result)
-
-        // todo success indicator on timeout
-        setProcessingState("")
-        // await navigate("/upload-success")
+    if (!title) {
+      result = false
+      console.log("Must enter title")
     }
 
-    function entryPageName() {
-        return entryPageTag[0].name
+    if (!cssValid) {
+      result = false
+      console.log("CSS is invalid")
     }
+
+    return result
+  }
+
+  async function getDataToUpload() {
+    if (!roamDataSupplier) {
+      return file
+    }
+
+    setProcessingState("Creating full data export")
+    const roamExport = await executeAfterDelay(0, roamDataSupplier)
+    console.log("Export done, performing filtering")
+    const {pages} = new RoamJsonQuery(roamExport, constructFilter()).getPagesToRender()
+    console.log("Export & filtering done")
+    return JSON.stringify(pages)
+  }
+
+  function constructFilter() {
+    return {
+      makeAllPagesPublic: allPagesPublic,
+      pagesToMakePublic: [entryPageName()],
+      makePagesWithTheseTagsPublic: publicTags.map(it => it.name),
+      makeBlocksWithTheseTagsPrivate: privateTags.map(it => it.name),
+    }
+  }
+
+  async function submit(e: FormEvent<HTMLDivElement>) {
+    e.preventDefault()
+    if (!isValid()) return
+
+    setProcessingState("Initiating upload")
+    const roamData = await getDataToUpload()
+    setProcessingState("Uploading the export to Roam Garden")
+    const [url, cssUrl] = await Promise.all([upload(roamData), upload(cssCode)])
+    const entryPage = entryPageName()
+    const payload = {
+      config: {
+        filter: constructFilter(),
+        siteTitle: title,
+        entryPage,
+        cssUrl,
+      },
+      dbUrl: url,
+    }
+    console.log("sending", payload)
+    // todo don't commit
+    const result = await API.put(gardenApi, "/garden", {body: payload})
+    console.log(result)
+
+    // todo success indicator on timeout
+    setProcessingState("")
+    // await navigate("/upload-success")
+  }
+
+  function entryPageName() {
+    return entryPageTag[0].name
+  }
 }
